ee459208d59f182a3e471d65eb7e67df5e61445f
Ishan Official <ishanofficial999@gmail.com>
2025-09-11 13:09:48 +0545

    Add project files.

---
+++ ConsoleAppIntern/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("My name is Khadga bahadur pariyar. I have completed my bachelor of software Engineering  from pokhara university.");
